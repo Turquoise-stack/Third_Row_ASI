317bd0a207865ad7b5eb88a1d56e0f3991df0afa
K1rovets <nurzhigitovkir@gmail.com>
2024-11-28 11:03:05 +0100

    Add requirements.txt and dummy python package

---
+++ package/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="asiLiverPackage",
+    version="0.1.0",
+    author="s22090",
+    author_email="s22090@pjwstk.edu.pl",
+    description="python package for the asi project",
+    long_description="",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=[],
+)
